92ca824c511776249897bda59c18fa874283ba7a
IraLysenko <arch_ira@ukr.net>
2022-09-11 15:06:17 +0200

    assessment2: get input values

--- assessment2/main.js
+++ assessment2/main.js
@@ -1,2 +1,24 @@
-import { encryptText } from './modules/encrypt.js';
+//import { encryptText } from './modules/encrypt.js';
+
+
+const alphabet = [...'abcdefghijklmnopqrstuvwxyz'];
+const option = document.getElementById('shift');
+const textInput = document.getElementById('plaintext');
+const encryptedTextArea = document.getElementById('encryptedText');
+
+option.addEventListener('change',
+    (e) => {
+    //console.log('option = ' + e.target.value);
+});
+
+textInput.addEventListener('keyup',
+    (e) => {
+        //console.log('text = ' + e.target.value);
+        let text = e.target.value;
+        encryptedTextArea.value = e.target.value;
+        //console.debug(encryptedTextArea.value);
+    });
+
+
+
 
